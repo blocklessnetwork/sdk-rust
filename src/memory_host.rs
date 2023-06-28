e1775708e8273b99211621273fc159cbe8b54705
Josiah Savary <hello@jsavary.com>
2023-06-27 22:36:19 -0500

    Create memory_host.rs

---
+++ src/memory_host.rs
@@ -0,0 +1,7 @@
+    #[link(wasm_import_module = "blockless_memory")]
+    extern "C" {
+        #[link_name = "memory_read"]
+        pub(crate) fn memory_read(buf: *mut u8, len: u32, num: *mut u32) -> u32;
+        #[link_name = "env_var_read"]
+        pub(crate) fn env_var_read(buf: *mut u8, len: u32, num: *mut u32) -> u32;
+    }
