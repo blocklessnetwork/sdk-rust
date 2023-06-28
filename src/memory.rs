cf834d8ae5e93ba054b9c01629568f73792b4aee
Josiah Savary <hello@jsavary.com>
2023-06-27 22:41:02 -0500

    Add read_env_vars

--- src/memory.rs
+++ src/memory.rs
@@ -9,3 +9,13 @@ pub fn read_stdin(buf: &mut [u8]) -> std::io::Result<u32> {
     let err = std::io::Error::from_raw_os_error(errno as i32);
     Err(err)
 }
+
+pub fn read_env_vars(buf: &mut [u8]) -> std::io::Result<u32> {
+    let mut len = 0;
+    let errno = unsafe { env_var_read(buf.as_mut_ptr(), buf.len() as _, &mut len) };
+    if errno == 0 {
+      return Ok(len);
+    }
+    let err = std::io::Error::from_raw_os_error(errno as i32);
+    Err(err)
+}
