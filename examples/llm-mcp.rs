7835c9cd8c10c689f643a84585f56d830134a0f7
z <zees-dev@users.noreply.github.com>
2025-04-15 17:57:21 +1200

    added mcp example

---
+++ examples/llm-mcp.rs
@@ -0,0 +1,32 @@
+use blockless_sdk::*;
+
+/// This example demonstrates how to use the Blockless SDK to interact with two different LLM models
+/// and use MCP to call the tools.
+
+fn main() {
+    // large model
+    let mut llm = BlocklessLlm::new(SupportedModels::Custom(
+        "Llama-3.1-8B-Instruct-q4f16_1-MLC".to_string(),
+    ))
+    .unwrap();
+
+    // Assume we have two tools running on different ports
+    // 1. http://localhost:3001/sse - add
+    // 2. http://localhost:3002/sse - multiply
+    llm.set_options(LlmOptions {
+        tools_sse_urls: vec![
+            "http://localhost:3001/sse".to_string(),
+            "http://localhost:3002/sse".to_string(),
+        ],
+        ..Default::default()
+    })
+    .unwrap();
+
+    let response = llm
+        .chat_request("Add the following numbers: 1215, 2213")
+        .unwrap();
+    println!("llm Response: {}", response);
+
+    let response = llm.chat_request("Multiply 1215 by 2213").unwrap();
+    println!("llm Response: {}", response);
+}
